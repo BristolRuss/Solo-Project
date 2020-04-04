b37b9f19c69d9fb9183c47ef27fad0c0f693df2a
BristolRuss <BristolRuss@gmail.com>
2020-04-04 16:56:38 +0100

    added Hash Router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../App.css';
+import { HashRouter as Router } from "react-router-dom";
 import Header from '../Header';
 import Settings from '../Settings';
 import Players from '../Players';
@@ -9,18 +10,20 @@ function App({settingsSaved, matchesUnderway}) {
   return (
     <div className="App">
       <div className="Wrapper">
-        <Header />
-        {/* This is checking if the settings have been saved. If they haven't the settings component displays. If true
-        the app contines. */}
-        {!settingsSaved ? 
-          <Settings /> 
-        :
-        // if settings have been saved then this checks if the pairings have been confirmed
-          !matchesUnderway ?
-            <Players />
-            :
-            <MatchUps />
-        }
+        <Router>
+          <Header />
+          {/* This is checking if the settings have been saved. If they haven't the settings component displays. If true
+          the app contines. */}
+          {!settingsSaved ? 
+            <Settings /> 
+          :
+          // if settings have been saved then this checks if the pairings have been confirmed
+            !matchesUnderway ?
+              <Players />
+              :
+              <MatchUps />
+          }
+        </Router>
       </div>
     </div>
   );
