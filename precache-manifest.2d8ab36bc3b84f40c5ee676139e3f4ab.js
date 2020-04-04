d121c8070887dd012e37182d1abeeec137dd2588
BristolRuss <BristolRuss@gmail.com>
2020-04-04 16:40:40 +0100

    Updates

---
+++ precache-manifest.2d8ab36bc3b84f40c5ee676139e3f4ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e23838f1cb9a0d0e4431037b8eaaa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "291c0fdc690168f2eb1b",
+    "url": "/static/css/main.ba7e8d2a.chunk.css"
+  },
+  {
+    "revision": "4b7fba1a088a2cb12528",
+    "url": "/static/js/2.597f268f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.597f268f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291c0fdc690168f2eb1b",
+    "url": "/static/js/main.d4dbb93e.chunk.js"
+  },
+  {
+    "revision": "2bd89dfe55fffd9e0398",
+    "url": "/static/js/runtime-main.964f42d3.js"
+  },
+  {
+    "revision": "e700bb7ac8c6dfebd5a09632fffc1bef",
+    "url": "/static/media/Optimized-30426.e700bb7a.jpg"
+  },
+  {
+    "revision": "e3bed5f7e9b4a52b8c226bfb34d44eb0",
+    "url": "/static/media/SqueakyChalkSound.e3bed5f7.ttf"
+  },
+  {
+    "revision": "b176de4cfb49ec6073a87ac080287836",
+    "url": "/static/media/blackboard.b176de4c.png"
+  }
+]);
